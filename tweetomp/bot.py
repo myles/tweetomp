8d91f49345c359299fdec313b93a606f001820fe
Myles Braithwaite <me@mylesbraithwaite.com>
2009-10-26 15:27:22 -0400

    Fixed up some stupid issues.

--- tweetomp/bot.py
+++ tweetomp/bot.py
@@ -96,7 +96,7 @@ def main():
 	
 	logging.basicConfig(
 		level = level[config.get('tweetomp', 'verbosity')],
-		format="%Y-%m-%d %H:%M:%S %(name)s: %(levelname)s: %(message)s",
+		format="%(asctime)s: %(name)s: %(levelname)s: %(message)s",
 		filename=log_file)
 	
 	t = TweetBot(
